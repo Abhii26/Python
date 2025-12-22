e76c606e31c25e326bde4e0a32635d8de7707eaa
ABHISHEK <135054435+Abhii26@users.noreply.github.com>
2025-12-22 22:48:06 +0530

    Delete folder

--- 05_Function/functques3.py
+++
@@ -1,9 +0,0 @@
-""" Ques3. Polymorphism in Functions
-Problem: Write a function multiply that multiplies two numbers, but can also accept and multiply strings."""
-
-def multiply(a,b):
-    c = a * b
-    return c 
-
-x= multiply(5, "Dj")
-print(x)
